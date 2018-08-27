ac0d6c541327a51ad4eced1e58fce2e16c38c2a2
Kenneth Ankomah <kenankomah@live.co.uk>
2018-08-27 16:10:13 +0100

    2nd heroku commit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+//add this file to .gitignore
+
+module.exports={
+  google:{
+    clientID:'438391959798-9aiegmb3ggfdnra194mu8qfd85bts555.apps.googleusercontent.com',
+    clientSecret:'J02EMa8s-kUFP7TgAmt3AmRP'
+  },
+  mongodb:{
+    dbURI:'mongodb://kenankomah:quantum12@ds133630.mlab.com:33630/kankomah'
+  },
+  session:{
+    cookieKey:'niemandkanndaserraten'
+  }
+};
