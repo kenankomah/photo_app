4914fa8555622ba88583c837249e5d3627b8529a
Kenneth Ankomah <kenankomah@live.co.uk>
2018-08-27 17:37:36 +0100

    another commit

--- config/keys.js
+++ config/keys.js
@@ -6,7 +6,8 @@ module.exports={
     clientSecret:'J02EMa8s-kUFP7TgAmt3AmRP'
   },
   mongodb:{
-    dbURI:'mongodb://kenankomah:quantum12@ds133630.mlab.com:33630/kankomah'
+    //dbURI:'mongodb://kenankomah:quantum12@ds133630.mlab.com:33630/kankomah'
+     dbURI:'mongodb://heroku_8zh1rdd6:<dbpassword>@ds133632.mlab.com:33632/heroku_8zh1rdd6'
   },
   session:{
     cookieKey:'niemandkanndaserraten'
